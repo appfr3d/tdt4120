d2b996796f93a182758d0e760ea03b0cee79d8ec
Alfred Lieth Årøe <alfredla@Alfreds-MacBook-Pro.local>
2018-09-06 12:12:33 +0200

    Ferdig med oppgave a

---
+++ øving_3/bisect.jl
@@ -0,0 +1,43 @@
+function bisect_left(A, p, r, v)
+    i = p
+    if p < r
+        # finner midten
+        q = floor(Int, (p+r)/2)  # floor() er en innebygd funksjon som runder ned. ceil() runder opp.
+        
+        # hvis verdien er mindre enn midtverdien
+        if v <= A[q]
+        	# kjør videre på den venstre delen
+            i = bisect_left(A, p, q, v)
+       	else
+       		# kjør videre på den høyre delen
+            i = bisect_left(A, q+1, r, v)
+        end
+    end
+    return i
+end
+
+
+A = [1,4,2,7,5,3]
+print(bisect_left(A, 1, length(A), 5), '\n')
+
+function bisect_right(A, p, r, v)
+    i = p
+    if p < r
+        # finner midten
+        q = floor(Int, (p+r)/2)  # floor() er en innebygd funksjon som runder ned. ceil() runder opp.
+        
+        # hvis verdien er mindre enn midtverdien
+        # ikke lengre <= men <
+        if v < A[q]
+            i = bisect_right(A, p, q, v)
+       	else
+            i = bisect_right(A, q+1, r, v)
+        end
+    end
+    return i
+end
+
+A = [1,4,2,7,5,3]
+print(bisect_right(A, 1, length(A), 5), '\n')
+
+
---
+++ øving_3/median.jl
@@ -0,0 +1,11 @@
+function partition(A, p, r)
+
+end
+
+function algdat_sort!(A)
+    
+end
+
+function find_median(A, lower, upper)
+    
+end
