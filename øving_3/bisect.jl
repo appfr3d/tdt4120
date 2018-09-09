14b08ed3143a2c919e7c780d2891be80f2aa1b4f
Alfred Lieth Årøe <alfredla@vpn-223227.vpn-s.ntnu.no>
2018-09-09 22:28:35 +0200

    Øving 3 ferdig

--- øving_3/bisect.jl
+++ øving_3/bisect.jl
@@ -2,29 +2,28 @@ function bisect_left(A, p, r, v)
     i = p
     if p < r
         # finner midten
-        q = floor(Int, (p+r)/2)  # floor() er en innebygd funksjon som runder ned. ceil() runder opp.
+        q = floor(Int, (p+r)/2)
         
         # hvis verdien er mindre enn midtverdien
         if v <= A[q]
-        	# kjør videre på den venstre delen
+            # kjør videre på den venstre delen
             i = bisect_left(A, p, q, v)
        	else
-       		# kjør videre på den høyre delen
+            # kjør videre på den høyre delen
             i = bisect_left(A, q+1, r, v)
         end
     end
     return i
 end
 
-
-A = [1,4,2,7,5,3]
+A = [1,2,3,4,5,6]
 print(bisect_left(A, 1, length(A), 5), '\n')
 
 function bisect_right(A, p, r, v)
     i = p
     if p < r
         # finner midten
-        q = floor(Int, (p+r)/2)  # floor() er en innebygd funksjon som runder ned. ceil() runder opp.
+        q = floor(Int, (p+r)/2)
         
         # hvis verdien er mindre enn midtverdien
         # ikke lengre <= men <
@@ -37,7 +36,7 @@ function bisect_right(A, p, r, v)
     return i
 end
 
-A = [1,4,2,7,5,3]
+A = [1,2,3,4,5,6]
 print(bisect_right(A, 1, length(A), 5), '\n')
 
 
--- øving_3/median.jl
+++ øving_3/median.jl
@@ -1,11 +1,80 @@
-function partition(A, p, r)
+# Basert på pseudokode fra forelesning
+function partition!(A, p, r)
+	x = A[r]	# siste element i partisjonen
+	i = p-1		# indexen før p
 
+	# gå gjennom partisjonen
+	for j in p:(r-1)
+	    if A[j] <= x
+	        i += 1
+	        tmp = A[i]
+	        A[i] = A[j]
+	        A[j] = tmp
+	    end
+	end
+	tmp = A[r]
+	A[r] = A[i+1]
+	A[i+1] = tmp
+	return i+1
 end
 
+# Basert på pseudokode fra forelesning
+function quick_sort!(A, p, r)
+    if p < r
+        q = partition!(A, p, r)
+        quick_sort!(A, p, q-1)
+        quick_sort!(A, q+1, r)
+    end
+    return A
+end
+
+# Kaller quick_sort på riktig måte
 function algdat_sort!(A)
-    
+    return quick_sort!(A, 1, length(A))
 end
+
+
+# Binærsøkfunksjonene fra del a)
+function bisect_left(A, p, r, v)
+    i = p
+    if p < r
+        q = floor(Int, (p+r)/2)
+        if v <= A[q]
+            i = bisect_left(A, p, q, v)
+        else
+            i = bisect_left(A, q+1, r, v)
+        end
+    end
+    return i
+end
+
+function bisect_right(A, p, r, v)
+    i = p
+    if p < r
+        q = floor(Int, (p+r)/2)
+        if v < A[q]
+            i = bisect_right(A, p, q, v)
+       	else
+            i = bisect_right(A, q+1, r, v)
+        end
+    end
+    return i
+end
+
 
 function find_median(A, lower, upper)
-    
+    # Finn indexen til lower og upper
+    min_index = bisect_left(A, 1, length(A)+1, lower)
+    max_index = bisect_right(A, 1, length(A)+1, upper) - 1
+
+    # finner median
+    if (max_index-min_index+1)%2 == 0
+        # hvis partall, returner gjennomsnitt av de to i midten
+        i = floor(Int, (max_index-min_index)/2) 
+        i += min_index
+        return (A[i] + A[i+1])/2
+    else
+        # hvis oddetall, returner den i midten
+        return A[min_index + floor(Int, (max_index - min_index)/2)]
+    end
 end
